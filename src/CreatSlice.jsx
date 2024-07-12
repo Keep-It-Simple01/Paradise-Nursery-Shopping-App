5810b6709cb55c0edda190a91601797ba667a5e0
Keep-It-Simple01 <bmnyakupeta@gmail.com>
2024-07-12 17:51:30 -0400

    last commit

--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -3,7 +3,9 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CreatSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
+    totalQuantity: 0,
+    totalPrice: 0 // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
@@ -14,9 +16,32 @@ export const CreatSlice = createSlice({
         } else {
             state.items.push({ name, image, cost, quantity: 1 });
         }
+        state.totalQuantity++;
+        state.totalPrice += parseFloat(cost.substring(1));
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        const itemToRemove = state.items.find(item => item.name === action.payload);
+        if (itemToRemove) {
+            state.totalQuantity -= itemToRemove.quantity;
+            state.totalPrice -= parseFloat(itemToRemove.cost.substring(1)) * itemToRemove.quantity;
+            state.items = state.items.filter(item => item.name !== action.payload);
+        }
+    },
+    incrementQuantity: (state, action) => {
+        const itemToIncrement = state.items.find(item => item.name === action.payload);
+        if (itemToIncrement) {
+            itemToIncrement.quantity++;
+            state.totalQuantity++;
+            state.totalPrice += parseFloat(itemToIncrement.cost.substring(1));
+        }
+    },
+    decrementQuantity: (state, action) => {
+        const itemToDecrement = state.items.find(item => item.name === action.payload);
+        if (itemToDecrement && itemToDecrement.quantity > 1) {
+            itemToDecrement.quantity--;
+            state.totalQuantity--;
+            state.totalPrice -= parseFloat(itemToDecrement.cost.substring(1));
+        }
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
